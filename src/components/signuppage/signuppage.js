85515b2c2ef8429417bb988485cb8a5a59505392
shitalkhante <shitalkhante786@gmail.com>
2022-12-13 14:00:18 +0530

    adding update

--- src/components/signuppage/signuppage.js
+++ src/components/signuppage/signuppage.js
@@ -7,68 +7,55 @@ import "./signup.css";
 
 const Signuppage = () => {
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [confirmpassword, setConfirmpassword] = useState("")
-    const navigate = useNavigate()
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmpassword, setConfirmpassword] = useState("");
+    const navigate = useNavigate();
 
 
     const handleOnSubmit = async (e) => {
-
-
-        const formdata = new FormData(e.target)
-        const email = formdata.get("email")
-        const password = formdata.get("password")
-        const confirmpassword = formdata.get("confirmpassword")
-
-
-        await axios.post('https://contact-manager-zxwn.onrender.com/signup', {
-            email: email,
-            password: password,
-            confirmpassword: confirmpassword
-
-        }).then(res => res.json(),
-            res => alert(res.data),
-            alert("saved success fully"),
-            navigate('/')
-        )
-            .then(
-                (data) => {
-                    console.log(data);
-                }
-            )
-
-            .catch(err => {
-                console.log(err)
+        try{
+            await axios.post('http://localhost:8000/signup', {
+                email: email,
+                password: password
+            },{headers:{
+                "Content-Type":"application/json"
+            }}).then(res=>{
+                alert(res.data.msg);
+                navigate("/")
+            }).catch(data => {
+                    alert("hii " + data.data.msg);
             })
-
-
-
+            
+        }catch(err){
+            alert("error in saving");
+        }
     }
     return (
         <>
-        <div className="main-container">
-        <div className="signup-heading">
-               <h1>Registation Form</h1>
-               </div>
-            <div className="signup-container">
-                  
-                <form className='form' onSubmit={handleOnSubmit} >
-                    <input type="text" name='email' value={email} onChange={(e) => setEmail(e.target.value)} className='form-input2' placeholder='abcexample@gmail.com' required />
+            <div className="main-container">
+                <div className="signup-heading">
+                    <h1>Registation Form</h1>
+                </div>
+                <div className="signup-container">
+
+                    <div className='form'>
+                        <input type="text" name='email' value={email} onChange={(e) => setEmail(e.target.value)} className='form-input2' placeholder='abcexample@gmail.com' required />
 
 
-                    <input type="text" name='password' value={password} onChange={(e) => setPassword(e.target.value)} className='form-input2' placeholder='Password' required />
+                        <input type="text" name='password' value={password} onChange={(e) => setPassword(e.target.value)} className='form-input2' placeholder='Password' required />
 
-                    <input type="text" name='confirmpassword' value={confirmpassword} onChange={(e) => setConfirmpassword(e.target.value)} className='form-input2' placeholder='Confirm Password' required />
+                        <input type="text" name='confirmpassword' value={confirmpassword} onChange={(e) => setConfirmpassword(e.target.value)} className='form-input2' placeholder='Confirm Password' required />
 
-                    {password !== confirmpassword ? <p>password are not matchng</p> : null}
-                    <button className='btn-btn-register'>Register</button>
-                </form>
+                        {
+                            password !== confirmpassword ? <p>password are not matchng</p> : null
+                        }
+
+                        <button className='btn-btn-register' onClick={(e) => { handleOnSubmit(e)}}>Register</button>
+                    </div>
+                </div>
             </div>
 
-
-        </div>
-               
 
 
 
